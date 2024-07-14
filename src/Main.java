8f32988a8219c5b69465806ad42d73c7a6f8c9ab
= <=>
2024-07-14 18:28:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Yardimci.classVariable);
+        Yardimci nesne=new Yardimci();
+        System.out.println(nesne.instanceVariable);
+        //System.out.println(nesne.localVariable);
+    }
+}
---
+++ src/Yardimci.java
@@ -0,0 +1,7 @@
+public class Yardimci {
+    static int classVariable = 1;
+    int instanceVariable = 2;
+    public void num(){
+        int localVariable = 3;
+    }
+}
